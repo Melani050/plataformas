b4d99eaf34e78fd924210433690903ca41199cce
Melani050 <tina.meli25@gmail.com>
2023-09-11 12:29:43 -0300

    defini el modelo profesor

---
+++ models/profesor.js
@@ -0,0 +1,24 @@
+class Profesor extends Model {}
+
+// DEFINO EL MODELO PROFESOR
+Profesor.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    }
+}, {
+    sequelize,
+    odelName: 'profesor'
+});
+
+module.exports = Profesor;
